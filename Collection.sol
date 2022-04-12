61ed3fe6c63f0cafe85f5bcb0a903009edf1c993
mwq07081997 <38372817+mwq07081997@users.noreply.github.com>
2022-04-12 08:23:34 +0800

    Update Collection.sol

--- Collection.sol
+++ Collection.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+
 // File: @openzeppelin/contracts/utils/Context.sol
 
 
@@ -1158,6 +1158,66 @@ contract ERC1155 is Context, ERC165, IERC1155, IERC1155MetadataURI {
     }
 }
 
+// File: @openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol
+
+
+// OpenZeppelin Contracts v4.4.1 (token/ERC1155/extensions/ERC1155Supply.sol)
+
+pragma solidity ^0.8.0;
+
+
+/**
+ * @dev Extension of ERC1155 that adds tracking of total supply per id.
+ *
+ * Useful for scenarios where Fungible and Non-fungible tokens have to be
+ * clearly identified. Note: While a totalSupply of 1 might mean the
+ * corresponding is an NFT, there is no guarantees that no other token with the
+ * same id are not going to be minted.
+ */
+abstract contract ERC1155Supply is ERC1155 {
+    mapping(uint256 => uint256) private _totalSupply;
+
+    /**
+     * @dev Total amount of tokens in with a given id.
+     */
+    function totalSupply(uint256 id) public view virtual returns (uint256) {
+        return _totalSupply[id];
+    }
+
+    /**
+     * @dev Indicates whether any token exist with a given id, or not.
+     */
+    function exists(uint256 id) public view virtual returns (bool) {
+        return ERC1155Supply.totalSupply(id) > 0;
+    }
+
+    /**
+     * @dev See {ERC1155-_beforeTokenTransfer}.
+     */
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+
+        if (from == address(0)) {
+            for (uint256 i = 0; i < ids.length; ++i) {
+                _totalSupply[ids[i]] += amounts[i];
+            }
+        }
+
+        if (to == address(0)) {
+            for (uint256 i = 0; i < ids.length; ++i) {
+                _totalSupply[ids[i]] -= amounts[i];
+            }
+        }
+    }
+}
+
 // File: @openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol
 
 
@@ -1212,7 +1272,8 @@ pragma solidity ^0.8.4;
 
 
 
-contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable {
+
+contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
 
     constructor() ERC1155("") {}
 
@@ -1276,7 +1337,7 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable {
 
         whenNotPaused
 
-        override
+        override(ERC1155, ERC1155Supply)
 
     {
 
@@ -1285,4 +1346,3 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable {
     }
 
 }
-
