b69d9fa3e88c6ff9b2e0066f1a193b5eb0d8de21
mwq07081997 <38372817+mwq07081997@users.noreply.github.com>
2022-12-18 13:31:05 +0800

    Update Cookie2Council.sol
    new update - Access Control

--- Cookie2Council.sol
+++ Cookie2Council.sol
@@ -12,8 +12,24 @@ contract Cookie2Council is Ownable, Pausable{
 
     constructor(string memory _name) {  
       name = _name;
+      _grantRole(ADMIN, msg.sender);
     }
 
+    // ROLE ACCESS
+    mapping(bytes32 => mapping(address => bool)) public roles;
+
+    bytes32 private constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
+    //bytes32 - 0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42
+    bytes32 private constant INDEXER = keccak256(abi.encodePacked("INDEXER"));
+    //bytes32 - 0x55e51077bf24628f06725d47e76e20668b7caf5302854ee4599bd926587af502
+
+    modifier onlyRole(bytes32 _role){
+      require(roles[_role][msg.sender], "Not authorized");
+      _;
+    }
+
+
+    /*
     //string transaction_ide;
     mapping(address => bool) whitelistedAddresses;
 
@@ -24,13 +40,28 @@ contract Cookie2Council is Ownable, Pausable{
       require(whitelistedAddresses[_address], "Whitelist: You need to be whitelisted");
       _;
     }
+    */
 
-    function addUser(address _addressToWhitelist) external onlyOwner {
+    // ROLE ACCESS FUNCTIONS
+    function _grantRole(bytes32 _role, address _account) internal {
+      roles[_role][_account] = true;
+    }
+
+    function grantRole(bytes32 _role, address _account) external onlyRole(ADMIN) {
+      _grantRole(_role, _account);
+    }
+
+    function revokeRole(bytes32 _role, address _account) external onlyRole(ADMIN) {
+      roles[_role][_account] = false;
+    }
+
+    /*
+    function addUser(address _addressToWhitelist) external onlyRole(ADMIN) {
       whitelistedAddresses[_addressToWhitelist] = true;
       list_of_addresses.push(_addressToWhitelist);
     }
 
-    function removeUser(address _addressToWhitelist) external onlyOwner {
+    function removeUser(address _addressToWhitelist) external onlyRole(ADMIN) {
       whitelistedAddresses[_addressToWhitelist] = false;
     }
 
@@ -38,9 +69,10 @@ contract Cookie2Council is Ownable, Pausable{
       bool userIsWhitelisted = whitelistedAddresses[_whitelistedAddress];
       return userIsWhitelisted;
     }
+    */
 
     // call to main contract to save logs
-    function logSignatures(address _contract, string[] calldata transaction_ids) external payable isWhitelisted(msg.sender) {
+    function logSignatures(address _contract, string[] calldata transaction_ids) external payable onlyRole(INDEXER) {
         // A's storage is set, B is not modified.
         string calldata transaction_id;
 
@@ -48,14 +80,14 @@ contract Cookie2Council is Ownable, Pausable{
             abi.encodeWithSignature("logSignatures(string[])", transaction_ids)
         );
 
-        require (success, "delegate call failed");
+        require (success, "delegate call failed: ");
     }
 
-    function pause() public isWhitelisted(msg.sender) {
+    function pause() public onlyRole(ADMIN) {
         _pause();
     }
 
-    function unpause() public isWhitelisted(msg.sender) {
+    function unpause() public onlyRole(ADMIN) {
         _unpause();
     }
     
